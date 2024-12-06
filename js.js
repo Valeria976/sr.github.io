173d48eb253860023bba4954354030884c39cd4f
Valeria976 <152829916+Valeria976@users.noreply.github.com>
2024-12-06 02:36:35 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.card');
+  
+    cards.forEach(card => {
+      card.addEventListener('click', () => {
+        const url = card.getAttribute('url');
+        if (url) {
+          window.location.href = url;
+        }
+      });
+    });
+  });
+  function goBack() 
+  { window.history.back();  }  
